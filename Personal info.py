d02d701580a21ac161c7075197fb0651c2ea3233
VeranielZF <82501582+VeranielZF@users.noreply.github.com>
2021-04-14 16:29:52 +0800

    Add files via upload

---
+++ Personal info.py
@@ -0,0 +1,9 @@
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+location = input("Enter your location: ")
+age = input("Enter your age: ")
+
+space = ' '
+
+
+print(first+space+last+space+location+space+age)
